6583a4e1800f6c1927dddc514d5c960cd4aa3b2d
Jonathan Amiez <jonathan.amiez@gmail.com>
2013-06-25 01:01:16 +0200

    add raw communication support (ILO < 3) and refactor parsing

--- hp/ilo.rb
+++ hp/ilo.rb
@@ -1,3 +1,5 @@
+require 'socket'
+require 'openssl'
 require 'net/https'
 require 'nokogiri'
 require 'json'
@@ -12,49 +14,62 @@ module HP
 
       @ribcl_path = "/ribcl"
       @verify_ssl = false
+
+      # TODO better choice
+      @protocol = :http
     end
 
-    def do(command, params = {})
-      if RIBCL.has_key?(command.to_sym)
-        puts @hostname
-        xml_req = ribcl(RIBCL[command][:context], RIBCL[command][:mode], command)
-        puts xml_req
-        xml_resp = send_request(xml_req)
-      #  puts xml_resp
-      #  parse_response(xml_resp, command)
-      else
-        puts "No such command: #{command}"
+    # TODO more tests (args, etc)
+    def method_missing(name, *args, &block)
+      request = ribcl(RIBCL[name][:context], RIBCL[name][:mode], name)
+      response = case @protocol
+      when :http
+        send_http_request(request)
+      when :raw
+        send_raw_request(request)
       end
+      parse_response(response, name)
+    end
+
+    def respond_to(name)
+      RIBCL.has_key?(name) ? true : super
     end
 
     private
 
-    #def send_request(xml)
-    #  https = Net::HTTP.new(@hostname, @port)
-    #  https.use_ssl = true
-    #  https.verify_mode = OpenSSL::SSL::VERIFY_NONE unless @verify_ssl
-    #  response = https.post(@ribcl_path, xml)
-    #  response.body
-    #end
-
-    def send_request(xml)
-      puts http_post(xml)
-#      sock = TCPSocket.new(@hostname, @port)
-#
-#      #ssl_ctx = OpenSSL::SSL::SSLContext.new(:TLSv1)
-#      #ssl_ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
-#      #ssl_sock = OpenSSL::SSL::SSLSocket.new(sock) #, ssl_ctx)
-#      #ssl_sock.sync_close = true
-#      #ssl_sock.connect
-#
-#      puts "puts"
-#      sock.write(xml)
-#      puts "gets"
-#      while line = sock.gets
-#        puts line
-#      end
+    def send_http_request(xml)
+      https = Net::HTTP.new(@hostname, @port)
+      https.use_ssl = true
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE unless @verify_ssl
+      response = https.post(@ribcl_path, xml)
+      if response.is_a?(Net::HTTPNotFound)
+        @protocol = :raw
+        send_raw_request(xml)
+      else
+        response.body
+      end
     end
 
+    def send_raw_request(xml)
+      sock = TCPSocket.new(@hostname, @port)
+
+      ctx = OpenSSL::SSL::SSLContext.new(:TLSv1)
+      ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      ssl_sock = OpenSSL::SSL::SSLSocket.new(sock, ctx)
+      ssl_sock.sync_close = true
+      ssl_sock.connect
+
+      ssl_sock.puts("#{xml}\r\n")
+      response = ""
+      while line = ssl_sock.gets
+        response += line
+      end
+      ssl_sock.close
+
+      response
+    end
+
+    # TODO find sane notation for XML -> hash
     def parse_response(xml, command)
       # ILO sends back multiple XML documents, split by XML header
       messages = xml.split(/<\?xml.*?\?>\r?\n/)
@@ -67,23 +82,31 @@ module HP
       messages.each do |doc|
         xml_doc = Nokogiri::XML(doc){|cfg| cfg.nonet.noblanks}
 
-        begin
-          check_response_status(xml_doc)
-        rescue Exception => e
-          puts e.message
-        end
+        xml_doc.root.children.each do |node|
+          case node.name
+          when "RESPONSE"
+            begin
+              check_response_status(node)
+            rescue Exception => e
+              puts e.message
+            end
+          when "INFORM"
+            # OSEF
+          else # command result
+            if node.children.length > 0
+              node.children.each do |child|
+                if child.attributes.length == 1 and child.has_attribute?("VALUE")
+                  output[child.name.downcase] = parse_value(child.attr("VALUE"))
+                else
+                  output[child.name.downcase] = {}
+                  child.each do |key,val|
+                    output[child.name.downcase][key.downcase] = parse_value(val)
+                  end
+                end
+              end
+            end
 
-        nodes = xml_doc.xpath("//#{command.upcase}/child::node()")
-
-        next if nodes.empty?
-
-        nodes.each do |node|
-          if node.attributes.length == 1 and node.has_attribute?("VALUE")
-            output[node.name.downcase] = parse_value(node.attr("VALUE"))
-          else
-            output[node.name.downcase] = {}
-            node.each do |key,val|
-              output[node.name.downcase][key.downcase] = parse_value(val)
+            if node.attributes.length > 0
             end
           end
         end
@@ -105,15 +128,11 @@ module HP
         }
       end
 
-      builder.doc.root.to_s
+      builder.to_xml
     end
 
-    def check_response_status(xml_doc)
-      response = xml_doc.xpath("//RESPONSE")
-      unless response.empty?
-        n = response.first
-        raise "#{n.attr("MESSAGE")} (#{n.attr("STATUS")})" unless n.attr("STATUS").to_i(16) == 0
-      end
+    def check_response_status(node)
+      raise "#{node.attr("MESSAGE")} (#{node.attr("STATUS")})" unless node.attr("STATUS").to_i(16) == 0
     end
 
     def parse_value(value)
@@ -125,18 +144,6 @@ module HP
       else
         value
       end
-    end
-
-    def http_post(xml)
-      str = <<POST
-POST #{@ribcl_path} HTTP/1.1
-Host: localhost
-Content-Length: #{xml.length}
-Connection: close
-
-
-POST
-      str
     end
   end
 end
