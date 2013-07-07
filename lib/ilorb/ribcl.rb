0484a24fc86e27fd0592713b3ec32c6e21c382d1
Jonathan Amiez <jonathan.amiez@gmail.com>
2013-07-07 19:52:04 +0200

    make Command and Context inner classes of RIBCL

--- lib/ilorb/ribcl.rb
+++ lib/ilorb/ribcl.rb
@@ -34,102 +34,102 @@ module ILORb
       context.instance_eval(&block)
       merge!(context.commands)
     end
-  end
 
-  class Context
-    attr_reader :commands
+    class Context
+      attr_reader :commands
 
-    def initialize(name)
-      @name = name.to_sym
-      @commands = {}
-    end
-
-    private
-    # private methods are used by DSL
-
-    [:read, :write].each do |mode|
-      define_method "#{mode}_cmd" do |name, &block|
-        command = Command.new(name, @name, mode)
-        command.instance_eval(&block) if block
-        @commands[name] = command
+      def initialize(name)
+        @name = name.to_sym
+        @commands = {}
       end
-    end
-  end
 
-  class Command
-    attr_reader :name, :context, :mode
+      private
+      # private methods are used by DSL
 
-    def initialize(name, context, mode)
-      @name, @context, @mode = name.to_sym, context, mode
-      @attributes = []
-      @elements = {}
-      @text = nil
-      @supported = true
-    end
-
-    [:attributes, :elements, :text].each do |key|
-      define_method "get_#{key}" do
-        instance_variable_get("@#{key}")
-      end
-    end
-
-    def supported?
-      @supported
-    end
-
-    def map_elements
-      map = {}
-      @elements.each do |name, type|
-        if type == :value
-          map.store(name, [name, type])
-        elsif type.is_a?(Array)
-          type.each do |elt|
-            map.store("#{name}_#{elt}".to_sym, [name, elt])
-          end
-        else
-          map.store("#{name}_#{type}".to_sym, [name, type])
+      [:read, :write].each do |mode|
+        define_method "#{mode}_cmd" do |name, &block|
+          command = Command.new(name, @name, mode)
+          command.instance_eval(&block) if block
+          @commands[name] = command
         end
       end
-      map
     end
 
-    def get_params
-      params = []
-      params += @attributes
-      params << @text if @text
-      params += map_elements.keys
-      params
-    end
+    class Command
+      attr_reader :name, :context, :mode
 
-    private
-    # private methods are used by DSL
+      def initialize(name, context, mode)
+        @name, @context, @mode = name.to_sym, context, mode
+        @attributes = []
+        @elements = {}
+        @text = nil
+        @supported = true
+      end
 
-    def attributes(*params)
-      @attributes += params
-    end
+      [:attributes, :elements, :text].each do |key|
+        define_method "get_#{key}" do
+          instance_variable_get("@#{key}")
+        end
+      end
 
-    def elements(*params)
-      if @text.nil?
-        hash = {}
-        params.each do |param|
-          if param.is_a?(Hash)
-            hash.merge!(param)
+      def supported?
+        @supported
+      end
+
+      def map_elements
+        map = {}
+        @elements.each do |name, type|
+          if type == :value
+            map.store(name, [name, type])
+          elsif type.is_a?(Array)
+            type.each do |elt|
+              map.store("#{name}_#{elt}".to_sym, [name, elt])
+            end
           else
-            hash.store(param, :value)
+            map.store("#{name}_#{type}".to_sym, [name, type])
           end
         end
-        @elements.merge!(hash)
+        map
       end
-    end
 
-    def text(param)
-      if @elements.empty?
-        @text = param
+      def get_params
+        params = []
+        params += @attributes
+        params << @text if @text
+        params += map_elements.keys
+        params
       end
-    end
 
-    def not_implemented
-      @supported = false
+      private
+      # private methods are used by DSL
+
+      def attributes(*params)
+        @attributes += params
+      end
+
+      def elements(*params)
+        if @text.nil?
+          hash = {}
+          params.each do |param|
+            if param.is_a?(Hash)
+              hash.merge!(param)
+            else
+              hash.store(param, :value)
+            end
+          end
+          @elements.merge!(hash)
+        end
+      end
+
+      def text(param)
+        if @elements.empty?
+          @text = param
+        end
+      end
+
+      def not_implemented
+        @supported = false
+      end
     end
   end
 end
