aa79e569741e50d98e23b3e2c9155a264731b920
Jonathan Amiez <jonathan.amiez@gmail.com>
2014-08-08 17:01:08 +0200

    update list of RIB_INFO, SERVER_INFO and USER_INFO methods

--- lib/ilorb/definitions/rib.rb
+++ lib/ilorb/definitions/rib.rb
@@ -27,7 +27,9 @@ context :rib_info do
 
   write_cmd :ahs_clear_data
   write_cmd :brownout_recovery # > mod_global_settings
-  write_cmd :certificate_signing_request
+  write_cmd :certificate_signing_request do
+    elements :csr_state, :csr_country, :csr_locality, :csr_organization, :csr_organizational_unit, :csr_common_name
+  end
   write_cmd :clear_eventlog
   write_cmd :computer_lock do
     not_implemented
@@ -41,8 +43,7 @@ context :rib_info do
   write_cmd :hotkey_config do
     not_implemented
   end
-  write_cmd :import_certificate do #TODO
-    not_implemented
+  write_cmd :import_certificate do
     text :certificate
   end
   write_cmd :insert_virtual_media do
@@ -51,11 +52,18 @@ context :rib_info do
   write_cmd :license do
     elements :activate => :key
   end
-  write_cmd :mod_global_settings do #TODO
-    not_implemented
+  write_cmd :mod_global_settings do
+    elements :session_timeout, :f8_prompt_enabled, :http_port, :https_port, :remote_console_port, :min_password, :ilo_funct_enabled, :virtual_media_port, :f8_login_required, :enforce_aes, :authentication_failure_logging, :ssh_status, :ssh_port, :serial_cli_status, :serial_cli_speed, :rbsu_post_ip, :snmp_access_enabled, :snmp_port, :snmp_trap_port, :remote_syslog_enable, :remote_syslog_port, :remote_syslog_server_address, :alertmail_enable, :alertmail_email_address, :alertmail_sender_domain, :alertmail_smtp_server, :ipmi_dcmi_over_lan_enabled, :vsp_log_enable
   end
-  write_cmd :mod_network_settings do #TODO
-    not_implemented
+  write_cmd :mod_network_settings do
+    elements :enable_nic, :nic_speed, :full_duplex, :speed_autoselect, :ping_gateway,
+             :shared_network_port, :vlan_enabled, :vlan_id,
+             :dhcp_enable, :dhcp_domain_name, :dhcp_gateway, :dhcp_dns_server, :dhcp_wins_server, :dhcp_static_route, :dhcp_sntp_settings,
+             :ip_address, :subnet_mask, :gateway_ip_address, :dns_name, :domain_name,
+             :prim_dns_server, :sec_dns_server, :ter_dns_server, :reg_ddns_server,
+             :prim_wins_server, :sec_wins_server, :reg_wins_server,
+             :sntp_server1, :sntp_server2, :timezone,
+             :enclosure_ip_enable, :web_agent_ip_address
   end
   write_cmd :mod_snmp_im_settings do #TODO
     not_implemented
--- lib/ilorb/definitions/server.rb
+++ lib/ilorb/definitions/server.rb
@@ -1,51 +1,95 @@
 context :server_info do
-  read_cmd :get_embedded_health
-  read_cmd :get_host_power_saver_status
-  read_cmd :get_host_power_status
-  read_cmd :get_host_pwr_micro_ver
-  read_cmd :get_one_time_boot
-  read_cmd :get_persistent_boot
-  read_cmd :get_power_cap
-  read_cmd :get_power_readings
-  read_cmd :get_pwreg
-  read_cmd :get_server_auto_pwr
-  read_cmd :get_server_name
-  read_cmd :get_server_power_on_time
-  read_cmd :get_uid_status
+  read_cmd :get_tpm_status
 
-  write_cmd :clear_server_power_on_time
-  write_cmd :cold_boot_server
-  write_cmd :hold_pwr_btn do
-    attributes :toggle
-  end
-  write_cmd :press_power_btn
-  write_cmd :reset_server
-  write_cmd :server_auto_pwr do
+  read_cmd :get_current_boot_mode
+
+  read_cmd :get_pending_boot_mode
+  write_cmd :set_pending_boot_mode do
     attributes :value
   end
-  write_cmd :server_name do
-    attributes :value
-  end
-  write_cmd :set_host_power do
-    attributes :host_power
-  end
-  write_cmd :set_host_power_saver do
-    attributes :host_power_saver
-  end
-  write_cmd :set_one_time_boot do
-    attributes :value
-  end
+
+  read_cmd :get_persistent_boot
   write_cmd :set_persistent_boot do
     elements :device => [ :value ]
   end
-  write_cmd :set_power_cap do
-    attributes :power_cap
+
+  read_cmd :get_one_time_boot
+  write_cmd :set_one_time_boot do
+    attributes :value
   end
+
+  read_cmd :get_supported_boot_mode
+
+  read_cmd :get_server_name
+  write_cmd :server_name do
+    attributes :value
+  end
+
+  read_cmd :get_server_fqdn
+  write_cmd :server_fqdn do
+    attributes :value
+  end
+
+  read_cmd :get_smh_fqdn
+  write_cmd :smh_fqdn do
+    attributes :value
+  end
+
+  read_cmd :get_product_name
+
+  read_cmd :get_embedded_health
+
+  read_cmd :get_power_readings
+
+  read_cmd :get_pwreg
   write_cmd :set_pwreg do
     elements :pwralert => :type, :pwralert_settings => [:threshold, :duration]
   end
+
+  read_cmd :get_power_cap
+  write_cmd :set_power_cap do
+    attributes :power_cap
+  end
+
+  read_cmd :get_host_power_saver_status
+  write_cmd :set_host_power_saver do
+    attributes :host_power_saver
+  end
+
+  read_cmd :get_host_power_status
+  write_cmd :set_host_power do
+    attributes :host_power
+  end
+
+  read_cmd :get_host_pwr_micro_ver
+
+  write_cmd :reset_server
+
+  write_cmd :press_pwr_btn
+
+  write_cmd :hold_pwr_btn do
+    attributes :toggle
+  end
+
+  write_cmd :cold_boot_server
+
+  write_cmd :warm_boot_server
+
+  read_cmd :get_server_auto_pwr
+  write_cmd :server_auto_pwr do
+    attributes :value
+  end
+
+  read_cmd :get_uid_status
   write_cmd :uid_control do
     attributes :uid
   end
-  write_cmd :warm_boot_server
+
+  read_cmd :get_pers_mouse_keyboard_enabled
+  write_cmd :set_pers_mouse_keyboard_enabled do
+    attributes :value
+  end
+
+  read_cmd :get_server_power_on_time
+  write_cmd :clear_server_power_on_time
 end
